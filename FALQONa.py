28bfa0ee5123c8e3a2718867fe2b7e339eeee23b
JoshuaFoate <joshuafoate@gmail.com>
2023-08-17 17:20:23 -0500

    changes

--- FALQONa.py
+++ FALQONa.py
@@ -1,5 +1,4 @@
 import numpy as np
-import scipy
 from scipy import linalg
 from matplotlib import pyplot as plt
 
@@ -10,23 +9,26 @@ H_p = Z#hard change
 H_d = X#simple change
 psi_0 = np.transpose(np.array([[1.0/np.sqrt(2), -1.0/np.sqrt(2)]]))#simple change
 dt = 0.2
-U_p = scipy.linalg.expm(-1j*H_p*dt)
+U_p = linalg.expm(-1j*H_p*dt)
 comm = 1j*(H_d @ H_p - H_p @ H_d)
 n_steps = 20
 beta = [0]
-cost_function = [float(np.real(np.conj(np.transpose(psi_0)) @ H_p @ psi_0))]
+cost_function = [float(np.real(np.conj(np.transpose(psi_0)) @ H_p @ psi_0))]#
 
-U_d = scipy.linalg.expm(-1j*beta[0]*H_d*dt)
+U_d = linalg.expm(-1j*beta[0]*H_d*dt)
 psi = U_d @ U_p @ psi_0
-A = float(np.real(np.conj(np.transpose(psi)) @ comm @ psi))
+A = float(np.real(np.conj(np.transpose(psi)) @ comm @ psi))#
 beta.append(-A)
-cost_function.append(float(np.real(np.conj(np.transpose(psi)) @ H_p @ psi)))
+cost_function.append(float(np.real(np.conj(np.transpose(psi)) @ H_p @ psi)))#
 
 i = 1
 while i < n_steps:
-    U_d = scipy.linalg.expm(-1j*beta[i]*H_d*dt)
+    U_d = linalg.expm(-1j*beta[i]*H_d*dt)
     psi = U_d @ U_p @ psi
-    A = float((np.real(np.conj(np.transpose(psi)) @ comm @ psi)))
+    A = float((np.real(np.conj(np.transpose(psi)) @ comm @ psi)))#
     beta.append(-A)
-    cost_function.append(float(np.real(np.conj(np.transpose(psi)) @ H_p @ psi)))
+    cost_function.append(float(np.real(np.conj(np.transpose(psi)) @ H_p @ psi)))#
     i = i+1
+
+plt.plot(beta)
+plt.show()
